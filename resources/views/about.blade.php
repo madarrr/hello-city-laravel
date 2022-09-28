c66dba83cd3c442fdc65503cfe73196484bfd129
Home <home@Homes-MacBook-Pro.local>
2022-09-28 15:26:08 +0000

    add page about page and update link

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About Us | Hello city</title>
+    </head>
+    <body>
+    
+       <p> by THXG STONE</p>
+       <p> <a href="/">Revenir a la page d'acceuil </a></p>
+
+       <footer> 
+        <p> &copy; Copyright {{date('Y') }} &middot; <a href="/about-us">About Us</a> </p>
+       </footer>
+    </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function() {
+return view('home');
+});
+Route::get('/about-us',function(){
+return view('about'); 
 });
