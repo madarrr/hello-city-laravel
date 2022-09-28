1a60e968b48cdfca1a747b9be1887c20f7f4aee2
Home <home@Homes-MacBook-Pro.local>
2022-09-28 16:15:24 +0000

    layouts adding

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>About Us | Hello city</title>
-    </head>
-    <body>
-    
+@extends('base')
+@section('content')
        <p> by THXG STONE</p>
        <p> <a href="/">Revenir a la page d'acceuil </a></p>
-
-       <footer> 
-        <p> &copy; Copyright {{date('Y') }} &middot; </p>
-       </footer>
-    </body>
-
-</html>
+@endsection
