00e0bc14a98a14366baa7658334856d344fc994c
Home <home@Homes-MacBook-Pro.local>
2022-09-28 16:40:30 +0000

    manage the title of page with layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,7 @@
 @extends('base')
+@section('title')
+About Us | Hello city
+@endsection
 @section('content')
        <p> by THXG STONE</p>
        <p> <a href="/">Revenir a la page d'acceuil </a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,7 @@
 @extends('base')
+@section('title')
+Hello city
+@endsection
 @section('content')
        <h1>Hello from ivory coast !</h1>
        <p> it's currently {{date('h:i A') }}.</p>
