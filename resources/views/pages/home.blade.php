cc240782cd295136eed69c4f110455872ede90b6
Home <home@Homes-MacBook-Pro.local>
2022-09-30 20:27:23 +0000

    adding images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 {{config('app.name')}}
 @endsection
 @section('content')
+<img src="img/img1.png" alt="image bart simpson logo">
        <h1>Hello from ivory coast !</h1>
        <p> it's currently {{date('h:i A') }}.</p>
 
