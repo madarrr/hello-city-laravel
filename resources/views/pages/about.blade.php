ee35629a1c31880c6dbca9b8bee8a9e7c0af5361
Home <home@Homes-MacBook-Pro.local>
2022-10-01 00:27:29 +0000

    add style with tailwindcss

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,8 @@
 About Us | {{config('app.name')}}
 @endsection
 @section('content')
-       <p> by THXG STONE</p>
-       <p> <a href="{{ route('home') }}">Revenir a la page d'acceuil </a></p>
-       <img src="{{  asset('img/img1.png') }}" alt="image bart simpson">
+       <img src="{{  asset('/img/img1.png') }}" alt="image bart simpson" class='text-indigo=500 my-12 rounded-full hover:text-indigo-600 underline'>
+       <h2 class='text-gray-500 mb-5'>  <a href="{{ route('home') }}" class='text-indigo=500 hover:text-indigo-600 underline' >Revenir a la page d'acceuil </a></h2>
+       <p> Build with <span class='text-pink-500'>  &hearts; </span> by THXG STONE</p>
+     
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,8 +3,8 @@
 {{config('app.name')}}
 @endsection
 @section('content')
-<img src="{{asset ('/img/img1.png') }}" alt="image bart simpson logo">
-       <h1>Hello from ivory coast !</h1>
-       <p> it's currently {{date('h:i A') }}.</p>
+<img src="{{asset ('/img/img1.png') }}" alt="image bart simpson logo" class='mt-12 rounded-full shadow-md h=12'>
+       <h1 class='mt-5  text-3xl sm:text-5xl font-semibold text-indigo-600'>Hello from Ivory coast !</h1>
+       <p class='text-lg text-gray-800'> it's currently {{date('h:i A') }}.</p>
 
 @endsection 
